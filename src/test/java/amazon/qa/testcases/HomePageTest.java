146fdecc8b2f0b8041efe3de782b99e68e3989d9
snehaautomation <40503609+snehaautomation@users.noreply.github.com>
2018-06-25 06:53:49 -0700

    added minor changes

--- src/test/java/amazon/qa/testcases/HomePageTest.java
+++ src/test/java/amazon/qa/testcases/HomePageTest.java
@@ -1,5 +1,11 @@
 package amazon.qa.testcases;
 
+/*
+ * 
+ * author Sneha
+ * 
+ */
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
